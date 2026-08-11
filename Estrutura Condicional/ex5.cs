d74704d67c2298b25064be100677df8f2ba8e750
Pedro Lucas Lisboa <lisboapedro00@gmail.com>
2026-08-11 11:40:23 -0300

    2 periodo

---
+++ Estrutura Condicional/ex5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o preço da TV 32");
+            double tv = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o preço do DVD");
+            double dvd = double.Parse(Console.ReadLine());
+
+            if (tv + dvd < 1.300)
+            {
+                Console.WriteLine("Itens em promoção");
+            }
+
+            else
+            {
+
+                Console.WriteLine("Itens sem promoção");
+            }
+
+        }
+    }
+}
---
+++ Estrutura Condicional/ex7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero inteiro");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num %2 == 0)
+            {
+                Console.WriteLine("O numero digitado é par");
+            }
+
+            else
+            {
+                Console.WriteLine("O numero digitado é impar");
+            }
+        }
+    }
+}
