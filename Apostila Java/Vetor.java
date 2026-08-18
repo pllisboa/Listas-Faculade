30c014aa9e6547f6015d2579c218adffd5d44190
Pedro Lucas Lisboa <lisboapedro00@gmail.com>
2026-08-18 17:30:07 -0300

    Apostila Java

---
+++ Apostila Java/Vetor.java
@@ -0,0 +1,12 @@
+public class Vetor{
+
+     public static void main(String[]args){
+
+        int[] numeros ={10,20,30,40,50};
+        
+        for(int i = 0;i< numeros.length; i++){
+            System.out.println(numeros[i]);
+        }
+    }
+
+}
